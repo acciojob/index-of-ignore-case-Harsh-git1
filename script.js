8b3e77733819f5e2e6760063cc03ab81d22adb31
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2024-01-29 22:38:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-	for(let i=0;i<s1.length(); i++) {
-		alert(s1[i]);
-		
+	let i = 0;
+	let fam = s2[0];
+	while (i < s1.length) {
+		let fc = s1[i];
+
+		if(fc === fam) {
+			let sm = s1.substring(i, i+s2.length);
+			if(sm === s2) return i;
+		}
+		i++;
 	}
+	return -1;
 }
 indexOfIgnoreCase(s1, s2);
 // Please do not change the code below
