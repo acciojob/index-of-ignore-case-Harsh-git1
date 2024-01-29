4053f451c610a5a798b1bb25d552afbe861f5f61
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2024-01-29 20:00:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	for(let i=0;i<s1.length(); i++) {
+		alert(s1[i]);
+		
+	}
 }
-
+indexOfIgnoreCase(s1, s2);
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+//alert(indexOfIgnoreCase(s1, s2));
